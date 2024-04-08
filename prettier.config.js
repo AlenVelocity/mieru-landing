415202359ca7c23d7e8d8424aa056c8ee168d770
AlenVelocity <alenyohannan71@gmail.com>
2024-04-08 17:37:13 +0530

    Update prettier.config.js with new formatting options

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+  useTabs: true,
+  tabWidth: 4,
+  semi: false,
+  printWidth: 120
 };
 
 export default config;
